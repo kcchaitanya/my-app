15956bb54b0e9593a988bbff075bd1b9b88d18c7
Rasheeda <krashee@thoughtworks.com>
2021-08-10 19:07:58 +0530

    "Add test"

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,18 @@ test('renders Enter your details text', () => {
   const linkElement = screen.getByText(/Enter your details/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders Contact Form in App', () => {
+  render(<App />);
+  const country = screen.getByText('Country:');
+  const firstName = screen.getByText('First Name:');
+  const lastName = screen.getByText('Last Name:');
+  const email = screen.getByText('Email:');
+  const submit = screen.getByText('Submit');
+
+  expect(country).toBeInTheDocument();
+  expect(firstName).toBeInTheDocument();
+  expect(lastName).toBeInTheDocument();
+  expect(email).toBeInTheDocument();
+  expect(submit).toBeInTheDocument();
+});
